8a440c3a1487471c4ce0763be73ebe01297fab70
marcatata <jordan72able@gmail.com>
2017-02-24 16:09:52 +0200

    expansion of class CPhone

--- tema4/tema4/CPhone.cs
+++ tema4/tema4/CPhone.cs
@@ -1,28 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace tema4
+namespace gsmarena_scrape
 {
     [Serializable]
     public class CPhone
     {
-        //public int id;
-        public string brand;
-        public string model;
-        public string camera;
-        public string os;
-        public string osVersion;
-        public string memory;
-        
-        public string display;
-        public string cpu;
-        public string gpu;
-        public string chipset;
-        public string battery;
-        public string sensors;
-        //public CPhone() { }
+        public class CBody
+        {
+            public string Dimensions { set; get; }
+            public string Weight { set; get; }
+            public string SIM { set; get; }
+        }
+        public class CDisplay
+        {
+            public string Type { set; get; }
+            public string Size { set; get; }
+            public string Resolution { set; get; }
+            public string Protection { set; get; }
+        }
+        public class CPlatform
+        {
+            public string OS { set; get; }
+            public string Chipset { set; get; }
+            public string CPU { set; get; }
+            public string GPU { set; get; }
+        }
+        public class CMemory
+        {
+            public string CardSlot { set; get; }
+            public string Internal { set; get; }
+        }
+        public class CCamera
+        {
+            public string Primary { set; get; }
+            public string Features { set; get; }
+            public string Video { set; get; }
+            public string Secondary { set; get; }
+        }
+        public class CConnectivity
+        {
+            public string WLAN { set; get; }
+            public string Bluetooth { set; get; }
+            public string GPS { set; get; }
+            public string NFC { set; get; }
+            public string Radio { set; get; }
+            public string USB { set; get; }
+        }
+
+        public string Brand { set; get; }
+        public string Model { set; get; }
+        public CBody Body = new CBody();
+        public CDisplay Display = new CDisplay();
+        public CPlatform Platform = new CPlatform();
+        public CMemory Memory = new CMemory();
+        public CCamera Camera = new CCamera();
+        public CConnectivity Connectivity = new CConnectivity();
+        public string Sensors { set; get; }
+        public string Battery { set; get; }
     }
 }
